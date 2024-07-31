178c3354fa89dd611d7e09f743bc465987945c9c
Maslinda <maslinda.mansor@treasury.gov.my>
2024-07-31 12:40:12 +0800

    edit popup modal

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CommentStoreRequest;
+use Ramsey\Uuid\Uuid;
+use App\Models\Comment;
+
 
 class CommentController extends Controller
 {
-    //
+    public function store(CommentStoreRequest $request)
+    {
+        
+        $post = Post::find($request->post_id);
+
+        $comment = new Comment;
+        $comment -> uuid = Uuid::uuid4();
+        $comment -> post_id = $request->post_id;
+        $comment -> user_id = $request->author;
+        $comment -> content = $request->content;
+        $comment -> save();
+
+        return redirect()->route('posts.show',['post'=>$post->uuid]);
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,52 +13,105 @@
 
     <div class="container">
         @foreach ($posts as $post)
-        <div class="card mt-3">
-            <div class="card-body">
-                <h4 class="card-title"><a href="{{ route('posts.show',['post' => $post->uuid])}}" class="">{{ $post->title }}</a> <small> ~ {{ $post->user->name }} <i>{{$post->user->email}}</i></small></h4>
-                <p class="card-text">{{ $post->content }} <hr> <h3> Comments ({{ $post->comments->count() }})</h3>
-
-                    <form method="POST" name="commentForm" id="commentForm" action="" class="form-horizontal">
-
-                        @csrf
-
-                        <div class="row form-group {{ $errors->has('content') ? 'has-error' : '' }}">
-                            <label for="content" class="col-sm-3 control-label">Input</label>
-                            <div class="col-sm-9">
-                                <textarea id="content" name="content" class="form-control" required="required">Your text here... {{ old('content') }}</textarea>
-                                <small class="text-danger">{{ $errors->first('content') }}</small>
-                            </div>
-                        </div>
-
-                        <input type="hidden" name="post_id" value="{{ $post->id }}">
-                        <input type="hidden" name="user_id" value="{{ $post->user->id }}">
-                        <div class="btn-group float-right">
-                            <button type="submit" class="btn btn-success">Add Comment</button>
-                        </div>
-
-                    </form>
-
-                    @foreach ( $post->comments as $comment)
-
-                        {{-- <p>
-                            <strong>{{ $comment->user->name }} ({{ $comment->user->posts->count() }})</strong>
-                                    {{ $comment->content }}
-                        </p> --}}
-
-                        {{-- <p><strong>{{ $comment->user->name }}({{ $comment->user->posts->count() }})</strong> {{ $comment->content }}</p> --}}
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        <a href="{{ route('posts.show', ['post'=>$post->uuid]) }}"> {{ $post->title }}</a> <small>~ {{ $post->user->name }} (<i>{{ $post->user->email }}</i>)</small>    <button type="button" class="btn btn-primary btn-sm float-right edit" data-uuid="{{ $post->uuid }}">
+                            Edit
+                          </button>
+                    </h4>
+                    <p class="card-text">
+                        {{ $post->content }}
+                    </p>
+                    <hr>
+                    <h3>Comments ({{ $post->comments->count() }})</h3>
+                    @foreach ($post->comments as $comment)
                         <p><strong>{{ $comment->user->id.') '.$comment->user->name }}({{ $comment->user->posts->count() }})</strong> {{ $comment->content }}</p>
                     @endforeach
-                </p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="editPost-mdl" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Edit Post</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" name="updatePostForm" id="updatePostForm">
+                        @csrf
+                        <input type="hidden" name="uuid" value="" id="uuid">
+                        <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+                            <label for="title">Title</label>
+                            <input type="text" id="title" name="title" value="{{ old('title') }}"  class="form-control" required="required">
+                            <small class="text-danger">{{ $errors->first('title') }}</small>
+                        </div>
+
+                        <div class="form-group {{ $errors->has('author') ? 'has-error' : '' }}">
+                            <label for="author">User</label>
+                            <select id="author" name="author" class="form-control" required>
+                                <option value="">Select User</option>
+                                @foreach ($users as $key => $user)
+                                    <option {{ old('author') == $key ? 'selected' : '' }} value="{{ $key }}">{{ $user }}</option>
+                                @endforeach
+                            </select>
+                            <small class="text-danger">{{ $errors->first('author') }}</small>
+                        </div>
+
+                        <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
+                            <label for="content">Content</label>
+                            <textarea id="content" name="content" class="form-control" required="required">{{ old('content') }}</textarea>
+                            <small class="text-danger">{{ $errors->first('content') }}</small>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" id="btnSaveEditPost" class="btn btn-primary">Save</button>
+                </div>
             </div>
         </div>
-
-        @endforeach
     </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script>
+        $('.edit').click(function (e) {
+            e.preventDefault();
+            var uuid = $(this).data('uuid');
+
+            $.ajax({
+                type: "post",
+                url: "{{ route('posts.ajaxloadpost') }}",
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    uuid: uuid
+                },
+                dataType: "json",
+                success: function (response) {
+                    $('#uuid').val(response.uuid);
+                    $('#title').val(response.title);
+                    $('#author').val(response.author).change();
+                    $('#content').val(response.content);
+
+                    $('#editPost-mdl').modal('show');
+                }
+            });
+        });
+
+        $('#btnSaveEditPost').click(function (e) {
+            e.preventDefault();
+
+        });
+    </script>
   </body>
 </html>
